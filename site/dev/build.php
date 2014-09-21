1d5a482efb9ae86d2adde765309572fd9411c944
mukunda- <mukunda@mukunda.com>
2014-09-20 22:42:27 -0600

    javascript builder

--- site/dev/build.php
+++ site/dev/build.php
@@ -1,20 +1,25 @@
 <?php
 
-namespace Builder {
+namespace Builder;
+
+spl_autoload_register( function ($class) {
+	if( $class === "JShrink\Minifier" ) {
+		require_once 'libs/JShrink/Minifier.php';
+	}
+});
 
 require_once 'config.php';
 require_once 'logging.php';
 
 //-----------------------------------------------------------------------------
-function GetNewestFileTime( $folder ) {
-	$newtime = 0;
-	$files = glob( "$folder/*.scss", GLOB_NOSORT );
-	foreach( $files as &$file ) {
-		$newtime = max( filemtime( "$file" ), $newtime );
-	}
-	return $newtime;
+function GetNewestFileTime( $list ) { 
+	array_walk( $list, function( &$item, $key ) {
+		$item = filemtime($item);
+	});
+	$list[] = 0;
+	return max($list);
 }
-
+  
 //-----------------------------------------------------------------------------
 function Build() {
 	global $config;
@@ -23,30 +28,56 @@ function Build() {
 		mkdir( "logs", 0700 );
 	}
 	
-	LogInfo( "rebuilding css..." );
 	//-----------------------------------------------------------------------------
 	if( !file_exists( "min" ) ) {
 		mkdir( "min", 0755 );
 	}
 	
 	$css_target = 'min/style.min.css';
-	$newtime = GetNewestFileTime( 'css' );
+	$newtime = GetNewestFileTime( glob( 'css/*.scss', GLOB_NOSORT ) );
 	$gentime = file_exists( $css_target ) ? filemtime( $css_target ) : 0;
 
 	if( $newtime >= $gentime ) {
+		LogInfo( "building css..." );
 		if( $config->DebugMode() ) {
 			// debug mode, don't compress.
 			 
 			exec( "%RUBY%/bin/ruby \"%RUBY%/bin/sass\" css/main.scss $css_target", $output, $aaa ); 
-			  
+			
 		} else {
 			
 			exec( "%RUBY%/bin/ruby \"%RUBY%/bin/sass\" css/main.scss $css_target --style compressed"  );
 		}
 	}
+	
+	$js_target = 'min/scripts.min.js';
+	$js = glob( "js/*.js", GLOB_NOSORT );
+	$newtime = GetNewestFileTime( $js );
+	$gentime = file_exists( $js_target ) ? filemtime( $js_target ) : 0;
+	if( $newtime >= $gentime ) {
+		LogInfo( "building javascript..." );
+		
+		$code = "";
+		foreach( $js as $jsfile ) {
+			$code .= file_get_contents( $jsfile ) . "\n\n";
+		}
+		
+		if( $config->DebugMode() ) {
+			// debug mode, don't minify.
+			
+			file_put_contents( $js_target, $code );
+			
+		} else {
+			
+			
+			$code = \JShrink\Minifier::Minify( $code );	
+			file_put_contents( $js_target, $code );
+		}
+	}
+		
+	
 }
 
 Build();
-}
 
 ?>
