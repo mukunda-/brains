d001c04a5aa331f65e5e5413c846234b25826eee
mukunda- <mukunda@mukunda.com>
2014-10-23 14:27:42 -0600

    more

--- galaxy2/shader/fragment.glsl
+++ galaxy2/shader/fragment.glsl
@@ -1,12 +1,11 @@
 uniform sampler2D u_sampler;
 
 varying lowp vec2 f_uv;
-varying lowp vec2 f_center;
 varying lowp vec4 f_color;
 
 void main(void) {
 	lowp vec4 texel = texture2D( u_sampler, f_uv );
-	if( texel.a == 0.0 ) discard;
+	//if( texel.a == 0.0 ) discard;
 	texel *= f_color;
 	
 	gl_FragColor = texel;
--- galaxy2/shader/vertex.glsl
+++ galaxy2/shader/vertex.glsl
@@ -10,14 +10,13 @@ uniform vec2 u_translate;
 //uniform mat4 uPMatrix;
 
 varying lowp vec2 f_uv;
-varying lowp vec2 f_center;
 varying lowp vec4 f_color;
 
 
 void main(void) {
 	f_uv = a_texture;
 	f_color = a_color;
-	f_center = a_center;
+	
 	gl_Position = vec4( (a_position+u_translate) * u_screen_scale , 1.0, 1.0 );
 	
 }
