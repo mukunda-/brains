5e60fb685e5eff282ba244f60f67370c01702336
mukunda- <mukunda@mukunda.com>
2014-10-26 14:56:52 -0600

    a

--- site/template/template_info.php
+++ site/template/template_info.php
@@ -6,31 +6,33 @@
 		$svg->SetID( 'biglogo' );
 		$svg->Output();?>
 	</div>
-	<h1>Type in a word or phrase to begin.</h1>
+	<h1>Type in any word or phrase to begin.</h1>
 	
 	<?php include "template/template_newlink_raw.php" ?>
 	
-	<p>The Wordex is a creative resource that is built from human thoughts. 
-	Since you're a human, you can contribute to the project! 
-	Every <i>thought</i> you type in helps to build the database.</p>
-	
-	<p>To help out more, you can vote on existing <i>links</i>. 
-	When you search for a thought, it might already have links associated with it. 
-	If you can see a relation between a link's thought and the current thought
-	you are on, you should give it an upvote. If you don't see a relation, or if you just dislike a link, 
-	you should downvote it. Links with a low score sink 
-	to the bottom and may be removed later. You also put votes into the system just by using it normally;
-	if you type in the same thing as someone else, or click on a link, then an upvote is given.
-	
-	<p>Eventually, the Wordex will grow into a massive database of relations between
-	thoughts. It's kind of like a thesaurus, except with more than just synonyms. Actually, we aren't
-	looking for synonyms—that's what a thesaurus is for. For an example of what we <i>are</i> looking for: 
-	<i>baby</i> and <i>crib</i> are definitely not the same thing, but cribs are for babies! So, ideally, <i>baby </i>
-	and <i>crib</i> should form a <i>strong link</i>.</p>
-	
-	<p>This only works if a lot of people contribute, so you should tell your friends to put in a few words too!</p>
-	
-	<p>Want to explore the Wordex core? Visit the <i><a href="nebula/">Word Nebula</a><sup>(BETA)</sup></i>.</p>
+	<p>
+    The Wordex is a creative resource that's built from human input. 
+	  Every <i>thought</i> you type in helps to build the database.
+  </p>
+
+  <p>
+    Over time, the Wordex will grow into a massive reference of relations between
+    thoughts. It's kind of like a thesaurus, except we aren't only looking for synonyms.
+    For example, <i>baby</i> and <i>crib</i> 
+    are definitely not the same thing, but cribs are for babies, so ideally <i>baby</i>
+    and <i>crib</i> should form a <i>strong link</i>.
+  </p>
+
+  <p>
+    When you type in a thought, it
+    either creates a new link or strengthens an existing link. You can also vote
+    on links by clicking the arrows on them.
+    Upvote links you like, and downvote links that you dislike or contain nonsense.
+    Links with a low score sink to the bottom and may be removed later. The system only 
+    works if a lot of people contribute, so you should tell your friends to put in a few words too!
+  </p>
+ 
+  <p>You can also explore the Wordex core at the <i><a href="nebula/">Word Nebula</a><sup>(BETA)</sup></i>.</p>
 	
 	<h3>"A crowdsourced brainstorm"!</h3>
 	
